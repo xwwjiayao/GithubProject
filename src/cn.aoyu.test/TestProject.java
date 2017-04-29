0b21280d97d604b9f3ffe0553f9648c473c7ed62
xwwjiayao <xww13185993386>
2017-04-29 23:02:47 +0800

    添加项目

---
+++ src/cn.aoyu.test/TestProject.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Administrator on 2017-04-29.
+ */
+public class TestProject {
+
+    public static void main(String[] args) {
+        System.out.println("我是github提交测试");
+    }
+
+}
